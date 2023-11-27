794754bfd9afc3c7c1a2bd348ce833330218b7cf
allfred1 <franigoyt@gmail.com>
2023-11-27 19:15:33 +0500

    settings 0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="container">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam
+          eum error? Deleniti assumenda nemo libero minus eligendi dignissimos
+          quidem veritatis quisquam quis! Nulla consectetur reprehenderit sit
+          optio magni dolorem necessitatibus velit earum et, deleniti corrupti
+          eos numquam rerum? Esse voluptatem explicabo commodi ipsa officia
+          quaerat eius saepe unde, accusamus ad cupiditate reiciendis incidunt
+          quidem ullam in dolorum facilis necessitatibus eos vitae praesentium
+          accusantium sint. Expedita, deserunt. Atque consequatur repudiandae,
+          exercitationem iure ullam neque laborum est ducimus non in temporibus
+          ratione quod eligendi corrupti nihil reprehenderit ipsa sequi sint
+          consequuntur mollitia soluta, expedita alias. Maxime, qui, eius labore
+          voluptates culpa, et vel aliquam cum at reprehenderit neque
+          consequatur dolore nisi soluta quibusdam non pariatur vitae omnis sint
+          eos dolorem expedita harum! Delectus est provident deleniti neque
+          facilis. Sapiente iusto ipsam ipsa! Molestiae animi, vitae nulla
+          eligendi iste nam asperiores culpa laboriosam dolorum incidunt iure
+          amet, nihil laudantium rerum ipsa error labore obcaecati, natus
+          mollitia porro in id expedita omnis? Enim molestiae, aliquid deserunt
+          eius nemo sed obcaecati, facere et corporis facilis nesciunt quo
+          cupiditate quod eos officiis fugiat pariatur vero a consequuntur ullam
+          eveniet adipisci quia ad. Laborum, quidem rerum repellendus minima,
+          hic aspernatur atque amet eligendi quasi, eos eveniet illum itaque?
+          Repellendus veritatis iure quo, quia vel tempora! Ducimus vel esse
+          veritatis perspiciatis hic? Dolor repellendus inventore odit, laborum
+          animi consequatur eum illo! Rerum earum dolorem doloribus dicta
+          possimus ratione, pariatur expedita soluta blanditiis voluptatum
+          laborum omnis odio, explicabo minima in? Delectus impedit atque
+          incidunt laboriosam nemo temporibus quae error ex odit optio pariatur,
+          consequatur libero mollitia? Doloremque libero, quia reprehenderit
+          laboriosam nostrum illum incidunt similique dicta temporibus,
+          doloribus expedita dignissimos. Excepturi odit placeat, a laboriosam
+          magni eius iusto, exercitationem sapiente maxime modi facilis
+          temporibus cumque nulla voluptates alias inventore, repellat minima
+          corrupti recusandae delectus vitae impedit nihil id!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
